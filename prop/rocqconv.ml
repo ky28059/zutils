645161e32da969e9657f94c8a40d396f33c66442
Kevin Yu <kevinyu567@gmail.com>
2026-02-17 17:30:45 -0500

    Test custom layout nt to rocq

--- prop/rocqconv.ml
+++ prop/rocqconv.ml
@@ -3,10 +3,14 @@ open Typectx
 open Sugar
 module Nt = Normalty
 
+let layout_nt_to_rocq ty =
+  let () = Printf.printf "%s\n" (Nt.show_nt ty) in
+  ""
+
 (* TODO: dump to file *)
 let dump_primitives (ctx : Nt.t ctx) =
   List.iter
-    (fun { x; ty } -> Printf.printf "Axiom %s : %s.\n" x @@ Nt.layout_nt ty)
+    (fun { x; ty } -> Printf.printf "Axiom %s : %s.\n" x @@ layout_nt_to_rocq ty)
     @@ ctx_to_list ctx
 
 (* TODO: dump to file*)
